642d15212f723e7d3058bcff8e609bedee3e4bbd
Khushi Kavya <khushi.kavya6910@gmail.com>
2024-04-01 22:01:25 +0530

    forLoop

---
+++ 19.forLoop.py
@@ -0,0 +1,15 @@
+# for letter in "Khushi Kavya":
+#     print("Current Letter: ", letter)
+
+friends = ["Khushi", "Kavya", "Kriti", "Kritika"]
+# for friend in friends:
+#     print("Current Friend: ", friend)
+
+# for index in range(10):
+#     print("Current Index: ", index)
+
+# for index in range(3, 10):
+#     print("Current Index: ", index)
+
+for index in range(len(friends)):
+    print(friends[index])
