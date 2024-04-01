b69b28be0fb5cc936026ab549b5109a34ee19842
Khushi Kavya <khushi.kavya6910@gmail.com>
2024-04-01 19:46:31 +0530

    tuples

---
+++ 11.tuples.py
@@ -0,0 +1,8 @@
+#most common example of tuples is coordinates, let's say a series of x,y coordinates
+#TUPLES ARE IMMUTABLE i.e. cannot be modified or changes later once created
+#Tuples are used for data that should not be changed, like coordinates
+
+
+# coordinates = (4, 5)
+coordinates = [(4, 5), (6, 7), (80, 34)]  #list of tuples(coordinates)
+print(coordinates[0])
