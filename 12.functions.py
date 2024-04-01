e946d0ee29628fd5eba9c272a8fd2cd09c090fb7
Khushi Kavya <khushi.kavya6910@gmail.com>
2024-04-01 19:54:47 +0530

    functions

---
+++ 12.functions.py
@@ -0,0 +1,6 @@
+def say_hi(name, age):     #descriptive name like variables, indentation is imp otherwise considered not inside function
+    print("Hello " + name + "! " + "you are " + str(age))  #str() converts age to string
+
+print("Top")
+say_hi("khushi",20)
+print("Bottom") 
