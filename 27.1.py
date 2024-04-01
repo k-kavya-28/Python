168e4f30d1914754ec50833a050d75c7123d009f
Khushi Kavya <khushi.kavya6910@gmail.com>
2024-04-01 22:50:15 +0530

    classes_and_objects

--- 27.1.py
+++ 27.1.py
@@ -1,5 +1,6 @@
 from classes_and_objects import Student
-student1 = Student("Jim", "Business", 3.1, False)
-student2 = Student("Pam", "Art", 2.5, True)
-print(student1.name)
-print(student2.gpa)
+student1 = Student("Jim", "Business", 3.1)
+student2 = Student("Pam", "Art", 2.5)
+# print(student1.name)
+# print(student2.gpa)
+print(student1.on_honor_roll())
